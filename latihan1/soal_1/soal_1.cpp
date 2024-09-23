b16bf8cd35dd35575b1fb2218be1e993c0f6e561
auliafathustsani <auliafathustsani@mail.ugm.ac.id>
2024-09-23 20:20:20 +0700

    Create soal_1.cpp

---
+++ latihan1/soal_1/soal_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Input : ";
+	cin>>n;
+	
+	int sum { 0 };
+	for (int i { 1 }; i <= n; ++i ){
+		sum += i ;
+	}
+
+	cout << "Output : Sum = " << sum;
+
+	return 0;
+}
